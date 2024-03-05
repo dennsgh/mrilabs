d0a09dde416964207aebc26474e9b4d519b7c404
Dennis - <dennis.dennis@capgemini.com>
2024-03-05 21:44:26 +0100

    moved frontend outside back in to src higher level

--- etc/env.py
+++ etc/env.py
@@ -23,9 +23,8 @@ else:
 
 data = os.path.join(working_dir, "data")
 logs = os.path.join(working_dir, "logs")
-assets = os.path.join(working_dir, "frontend", "assets")
+assets = os.path.join(working_dir, "assets")
 
-frontend_src = os.path.join(working_dir, "frontend", "src")
 src = os.path.join(working_dir, "src")
 
 if (
@@ -34,9 +33,9 @@ if (
     or ostype == "cygwin"
     or ostype == "msys"
 ):
-    pythonpath = f"{frontend_src}:{src}"
+    pythonpath = f"{src}"
 else:
-    pythonpath = f"{os.getenv('PYTHONPATH', '')}:{src}:{frontend_src}"
+    pythonpath = f"{os.getenv('PYTHONPATH', '')}:{src}"
 
 # Set environment variables
 os.environ["WORKINGDIR"] = working_dir
@@ -44,7 +43,6 @@ os.environ["CONFIG"] = config
 os.environ["DATA"] = data
 os.environ["LOGS"] = logs
 os.environ["ASSETS"] = assets
-os.environ["FRONTEND_SRC"] = frontend_src
 os.environ["SRC"] = src
 os.environ["PYTHONPATH"] = pythonpath
 
@@ -57,7 +55,7 @@ for var in env_vars:
             "-m",
             "dotenv",
             "-f",
-            f"{working_dir}/.env",
+            f".env",
             "set",
             var,
             os.getenv(var),
