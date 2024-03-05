498e6d77bdf4668591b409cdcc1211fb7a335c9a
Dennis - <dennis.dennis@capgemini.com>
2024-03-05 21:50:21 +0100

    pytest backin

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import signal
-
+from dotenv import load_dotenv
 from app import run_application, signal_handler
 
 if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, signal_handler)
     try:
+        load_dotenv()
         run_application()
     except KeyboardInterrupt:
         print("Exit signal detected.")
